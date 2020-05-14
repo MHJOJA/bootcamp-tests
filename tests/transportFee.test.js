2ea84934e470690c4819a7cc506a6ede5c0c35b4
MHJOJA <dmenzi694@gmail.com>
2020-05-14 16:05:57 +0200

    initial commit

--- tests/transportFee.test.js
+++ tests/transportFee.test.js
@@ -1,8 +1,15 @@
+
 describe("The transportFee function ", function(){
   
-    it ("should return R10", function() {
-        
-        assert.equal(R10, "for afternoon shift ");
-    }); 
+    it ("should return R20 for 'morning'", function() {
+            assert.equal('R20', transportFee("morning"));
+    });  
+    it ("should return R10 for 'afternoon'", function() {
+        assert.equal('R10', transportFee("afternoon"));
+});  
+it ("should return R0 for 'night'", function() {
+    assert.equal('free', transportFee("night"));
+});  
 });
+
 
