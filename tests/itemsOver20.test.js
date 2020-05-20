ea1a3555b9d17b41bd76b97fd9d0219a832cfa85
MHJOJA <dmenzi694@gmail.com>
2020-05-20 17:29:48 +0200

    initial comit

--- tests/itemsOver20.test.js
+++ tests/itemsOver20.test.js
@@ -2,8 +2,12 @@
 describe('findItemsOver20 function' , function(){
 
     it('should return the products that have a quantity higher than 20.' , function(){
+        assert.equal('20', findItemsOver('itemList'));
+
            });
+
            it('should return the products that have a quantity over 20.' , function(){
+            assert.equal('20', findItemsOver('itemList'));
           });
 
 });
--- tests/mostProfitableDepartment.test.js
+++ tests/mostProfitableDepartment.test.js
@@ -2,11 +2,11 @@
 describe('mostProfitableDepartment function' , function(){
 
     it('should return the most profitable department.' , function(){
-        assert.equal(' ', mostProfitableDepartment('sales'));
+        assert.equal('Thursday', mostProfitableDepartment('36116'));
 
            });
            it('should return the highest profitable department.' , function(){
-            assert.equal(' ', mostProfitableDepartment('sales'));
+            assert.equal('Thursday', mostProfitableDepartment('36116'));
 
           });
 
