4ca4daa45bc41e7624631f20a307dab1abac6c71
MHJOJA <dmenzi694@gmail.com>
2020-06-02 17:37:27 +0200

    initital commit

--- tests/capetownRegNumber.test.js
+++ tests/capetownRegNumber.test.js
@@ -4,8 +4,8 @@ describe('isFromCapeTown function', function(){
   it('should return all CA registration numbers', function(){
     assert.equal(true, isFromCapeTown('CA 123 254, CA'));
   });
-  it('should return all CA registration numbers', function(){
-    assert.equal(false, isFromCapeTown('CJ 158 277, CJ'));
+  it('should fail if  registration numbers dont start with CA', function(){
+    assert.equal(false, isFromCapeTown('CJ 158 277, ZN'));
   });
 
 
