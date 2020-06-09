85b8048a541eea7a719596011d13ee15b0a1a93b
MHJOJA <dmenzi694@gmail.com>
2020-06-09 10:13:49 +0200

    initital commit

--- tests/limpopoReg.test.js
+++ tests/limpopoReg.test.js
@@ -1,7 +1,7 @@
 
 describe('isFromlimpopo function', function(){
 
-  it('should return all the registration numbers ending with L', function(){
+  it('should return all L registration numbers', function(){
       assert.equal(true, isFromLimpopo('SXA 125 L, L '));
   });
   it('should fail if registration numbers do not end with L', function(){
